72c92c48df2a53762408081ca944c0cb0693d4bc
ananya33 <agrawalananya33@gmail.com>
2019-01-18 14:30:06 +0530

    code for preprocessing

---
+++ preprocess.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import json
+from entity.input_data import fromJson
+from util.preprocess_util import make_unigrams, make_bigrams
+from entity.processed_data import ProcessedData 
+from util.preprocess_util import splitParaIntoSentences
+
+with open('input-datasets/input_data.json') as file:
+    data = json.load(file)
+
+with open('config.json') as file:
+    config = json.load(file)
+
+numberOfNewsArticles = int(config["training-data-size"])
+newsArticles = []
+for i in range(numberOfNewsArticles):
+    newsArticles.append(fromJson(json.dumps(data["data"][i])))
+    
+print(len(newsArticles))
+processedArticles = []
+for article in newsArticles:
+    sentenceList = splitParaIntoSentences(article["body"])
+    unigrams = []
+    bigrams = []
+    for sentence in sentenceList:
+        unigrams += make_unigrams(sentence)
+        bigrams += make_bigrams(sentence)
+    #print(unigrams)
+    #print(bigrams)
+    processedArticles.append(ProcessedData(article["date"], article["headline"], article["label"], article["delta"], unigrams, bigrams))
+
+print(json.dumps({"result":[ob.__dict__ for ob in processedArticles]}))
+
+with open('input-datasets/processed_data.json', 'w') as outfile:
+    json.dump({"result":[ob.__dict__ for ob in processedArticles]}, outfile, indent=2)
--- util/preprocess_util.py
+++ util/preprocess_util.py
@@ -7,23 +7,24 @@ possible_suffixes_verb = [dobj]
 possible_prefixes_noun = [amod, neg]
 possible_suffixes_noun = []
 
+nlp = spacy.load('en_core_web_sm', disable=['ner', 'textcat'])
+
 """
 removes stopwords and punctuations
 """
-def remove_stopwords(line):
-    nlp = spacy.load('en_core_web_sm')
+def remove_stopwords(line):    
     doc = nlp(line)
     output=[]
     for token in doc:
         if token.is_stop == False and token.is_punct == False and token.is_currency == False and token.is_digit == False:
             output.append(token)
-    print(output)
+    #print(output)
     return output
 """
 
 """
 def make_unigrams(line):
-    nlp = spacy.load('en_core_web_sm')
+    #nlp = spacy.load('en_core_web_sm')
     doc = nlp(line)
     output = []
     for np in doc.noun_chunks:
@@ -31,10 +32,11 @@ def make_unigrams(line):
     for word in doc:
         if word.pos == VERB and word.pos != AUX:
             output.append(word.lemma_)
-    print(output)
+    #print(output)
+    return output
     
 def make_bigrams(line):
-    nlp = spacy.load('en_core_web_sm')
+    #nlp = spacy.load('en_core_web_sm')
     doc = nlp(line)
     output = []
     for word in doc:
@@ -48,4 +50,18 @@ def make_bigrams(line):
             for child in word.children:
                 if child.dep in possible_prefixes_noun:
                     output.append((child.lemma_, word.lemma_))
-    print(output)
+    #print(output)
+    return output
+    
+# split up a paragraph into sentences
+# using regular expressions
+def splitParaIntoSentences(paragraph):
+    ''' break a paragraph into sentences
+        and return a list '''
+    import re
+    # to split by multile characters
+
+    #   regular expressions are easiest (and fastest)
+    sentenceEnders = re.compile('[.!?]')
+    sentenceList = sentenceEnders.split(paragraph)
+    return sentenceList
